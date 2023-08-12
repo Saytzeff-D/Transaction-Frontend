64ecbbdf755b7cfc00419da8bc64ad65a4a54c72
David Atanda <ololadedavid15@gmail.com>
2023-08-12 15:08:15 +0100

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { HistoryComponent } from './history/history.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/admin', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'admin', component: SideNavComponent, children: [
     {path: 'dashboard', component: DashboardComponent},
     {path: 'history', component: HistoryComponent},
     {path: 'statistics', component: StatisticsComponent}
-  ]}
+  ]},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
