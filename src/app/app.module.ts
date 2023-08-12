6dcc01b2bc80029ca879be65af838bbaeca6e84f
David Atanda <ololadedavid15@gmail.com>
2023-08-12 08:37:33 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { HistoryComponent } from './history/history.component';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MaterialModule } from './material/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistoryComponent,
-    AddTransactionComponent
+    AddTransactionComponent,
+    SideNavComponent,
+    DashboardComponent,
+    StatisticsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
