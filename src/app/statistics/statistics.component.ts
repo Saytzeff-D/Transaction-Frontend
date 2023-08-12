37f87e0fdbcdccd90a9eedc8a23ae7aa43f16983
David Atanda <ololadedavid15@gmail.com>
2023-08-12 13:45:08 +0100

    statistics

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  public amount = 700
+
+  public lineChartData = [
+    {
+    data: [20000, 12500, 9000 ],    
+    barThickness: 17,
+    borderRadius: 5,
+    // backgroundColor: 'green',
+    // theme: 'red'
+    }  
+  ]
+  public chartLabels = [    
+    'Transfer',
+    'Withdrawals',
+    'Others'
+  ]
+  public chartOptions = {
+    responsive: true, 
+    // aspectRatio: 1.    
+  }
+
+  ngOnInit(): void {
+      
+  }
 
 }
