9ddacd32174ba68c092c533260391a6674f9cd48
David Atanda <ololadedavid15@gmail.com>
2023-08-14 16:43:00 +0100

    now logging in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RequestsService } from '../services/requests.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +10,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  public isLoading = true
+  public isLoading = false
   public loginForm = new FormGroup({
     email: new FormControl('', Validators.required),
     name: new FormControl('', Validators.required)
   })
+  public error = ''
 
-  constructor(public request: RequestsService) {}
+  constructor(public request: RequestsService, public snackbar: MatSnackBar, public router: Router) {}
 
   login(){
     if (this.loginForm.valid) {
       this.isLoading = true
-      this.request.login(this.loginForm.value).subscribe((res=>{}), (err=>{}))
+      this.request.login(this.loginForm.value).subscribe((res=>{
+        this.isLoading = false
+        this.router.navigate(['/admin/dashboard'])
+      }), (err=>{
+        console.log(err)
+        this.isLoading = false
+        err.statusText == 'Unknown Error' ? this.error = 'Check your internet connection' : this.error = err.error.message
+        this.snackbar.open(this.error, 'Close', { duration: 5000 })
+      }))
     } else {
       console.log('Invalid')
     }
