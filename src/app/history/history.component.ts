6f01284152ae0c1748e4187e445cb396a254b4fb
David Atanda <ololadedavid15@gmail.com>
2023-08-14 16:42:38 +0100

    now deleting transactions

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -10,13 +10,17 @@ import { RequestsService } from '../services/requests.service';
 })
 export class HistoryComponent implements OnInit, AfterViewInit {
   public isLoading = true
+  public isDeleting = true
   public error = ''
 
   public dataSource:any = []
   public filterHistory = ''
+  public offset:any
   
   constructor(public request: RequestsService) { }
   ngOnInit(): void {
+    this.offset = (new Date().getTimezoneOffset());
+    console.log(this.offset)
     this.request.history().subscribe((res:any)=>{
       this.isLoading = false
       this.dataSource = res.transactions
@@ -28,6 +32,14 @@ export class HistoryComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
     // this.dataSource.paginator = this.paginator;
+  }
+  delete(id:any){
+    console.log(id)
+    this.request.deleteHistory({id: id}).subscribe(res=>{
+      this.dataSource = this.dataSource.filter((each:any, i:any)=>(each._id !== id))
+    }, err=>{
+      console.log(err)
+    })
   }
 
 }
